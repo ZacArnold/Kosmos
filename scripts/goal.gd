299e988c9e23b32a831160950d0b06e2f027fd3d
Thy Creeper King <67208693+ThyCreeperKing@users.noreply.github.com>
2022-03-09 07:18:15 -0800

    Mar 9 - Player roatation directional movement and code cleanup

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -2,4 +2,4 @@ extends Area
 
 func _on_goal_body_entered(body):
 	if body.name == "player":
-		get_tree().change_scene("res://levels/level_1.tscn")
+		get_tree().change_scene("res://levels/level_2.tscn")
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
-#Var setup
+
+##########Variable Setup##########
 const MAXSPEED = 12
 const FRICTION = 0.075
 const ACCELERATION = 0.075
@@ -8,22 +9,42 @@ const JUMPPOWER = 30
 const GRAVITY = 98/1.5
 const PIVOTSPEED = 90
 var velocity = Vector3.ZERO
-var playerDirection = "-Z"
+var playerDirection = 0
 var rotateVelocity = Vector3.ZERO
 var pivotVelocity = Vector3.ZERO
+##################################
+
 
 func _physics_process(delta):
 	var inputVelocity = Vector3.ZERO
 	var inputRotateVelocity = Vector3.ZERO
 	var inputPivotVelocity = Vector3.ZERO
 	
+	
 	##########Rotation Control##########
 	if Input.is_action_just_pressed("camera_pivot_left"):
 		inputPivotVelocity.y -= 1
-		playerDirection = "+X"
+		playerDirection -= 1
+	elif Input.is_action_just_pressed("camera_pivot_left"):
+		playerDirection -= 1
+	elif Input.is_action_just_pressed("camera_pivot_left"):
+		playerDirection -= 1
+	elif Input.is_action_just_pressed("camera_pivot_left"):
+		playerDirection = 0
+	
 	if Input.is_action_just_pressed("camera_pivot_right"):
 		inputPivotVelocity.y += 1
-		playerDirection = "-X"
+		playerDirection += 1
+	elif Input.is_action_just_pressed("camera_pivot_right"):
+		playerDirection += 1
+	elif Input.is_action_just_pressed("camera_pivot_right"):
+		playerDirection += 1
+	elif Input.is_action_just_pressed("camera_pivot_right"):
+		playerDirection = 0
+	
+	#Keeps direction set within range
+	if playerDirection == 4 or playerDirection == -4:
+		playerDirection = 0
 	
 	#Velocity maxed at set speed
 	inputPivotVelocity = inputPivotVelocity.normalized() * PIVOTSPEED
@@ -39,63 +60,61 @@ func _physics_process(delta):
 	
 	#Rotates Camera
 	rotate_y(deg2rad(pivotVelocity.y))
+	####################################
+	
+	
+	##########Player Rotation Control##########
+	if Input.is_action_pressed("forward"):
+		inputRotateVelocity.x -= 1
+	if Input.is_action_pressed("backward"):
+		inputRotateVelocity.x += 1
+	if Input.is_action_pressed("left"):
+		inputRotateVelocity.z += 1
+	if Input.is_action_pressed("right"):
+		inputRotateVelocity.z -= 1
+	###########################################
 	
 	
 	##########2D Movement Control##########
-	if Input.is_action_pressed("forward"):
-		inputVelocity.z -= 1
-		inputRotateVelocity.x -= 1
-	if Input.is_action_pressed("backward"):
-		inputVelocity.z += 1
-		inputRotateVelocity.x += 1
-	if Input.is_action_pressed("left"):
-		inputVelocity.x -= 1
-		inputRotateVelocity.z += 1
-	if Input.is_action_pressed("right"):
-		inputVelocity.x += 1
-		inputRotateVelocity.z -= 1
+	if playerDirection == 0:
+		if Input.is_action_pressed("forward"):
+			inputVelocity.z -= 1
+		if Input.is_action_pressed("backward"):
+			inputVelocity.z += 1
+		if Input.is_action_pressed("left"):
+			inputVelocity.x -= 1
+		if Input.is_action_pressed("right"):
+			inputVelocity.x += 1
 	
-	if rotation_degrees.y < 180 and rotation_degrees.y > 0:
+	if playerDirection == 1 or playerDirection == -3:
 		if Input.is_action_pressed("forward"):
 			inputVelocity.x -= 1
-			inputRotateVelocity.x -= 1
 		if Input.is_action_pressed("backward"):
 			inputVelocity.x += 1
-			inputRotateVelocity.x += 1
 		if Input.is_action_pressed("left"):
-			inputVelocity.z -= 1
-			inputRotateVelocity.z += 1
-		if Input.is_action_pressed("right"):
 			inputVelocity.z += 1
-			inputRotateVelocity.z -= 1
+		if Input.is_action_pressed("right"):
+			inputVelocity.z -= 1
 	
-	if rotation_degrees.y < 0 and rotation_degrees.y > -180:
+	if playerDirection == -1 or playerDirection == 3:
 		if Input.is_action_pressed("forward"):
 			inputVelocity.x += 1
-			inputRotateVelocity.x -= 1
 		if Input.is_action_pressed("backward"):
 			inputVelocity.x -= 1
-			inputRotateVelocity.x += 1
 		if Input.is_action_pressed("left"):
-			inputVelocity.z += 1
-			inputRotateVelocity.z += 1
-		if Input.is_action_pressed("right"):
 			inputVelocity.z -= 1
-			inputRotateVelocity.z -= 1
+		if Input.is_action_pressed("right"):
+			inputVelocity.z += 1
 	
-	if rotation_degrees.y < -90 and rotation_degrees.y > 90:
+	if playerDirection == 2 or playerDirection == -2:
 		if Input.is_action_pressed("forward"):
 			inputVelocity.z += 1
-			inputRotateVelocity.x -= 1
 		if Input.is_action_pressed("backward"):
 			inputVelocity.z -= 1
-			inputRotateVelocity.x += 1
 		if Input.is_action_pressed("left"):
 			inputVelocity.x += 1
-			inputRotateVelocity.z += 1
 		if Input.is_action_pressed("right"):
 			inputVelocity.x -= 1
-			inputRotateVelocity.z -= 1
 	
 	#Velocity maxed at set speed
 	inputVelocity = inputVelocity.normalized() * MAXSPEED
@@ -110,14 +129,15 @@ func _physics_process(delta):
 		velocity = velocity.linear_interpolate(Vector3.ZERO, FRICTION)
 		rotateVelocity = rotateVelocity.linear_interpolate(Vector3.ZERO, FRICTION)
 	
-	#Jumping movement control
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = JUMPPOWER
-	
-	#Gravity
-	velocity.y -= GRAVITY * delta
-	
 	#Moves and rotates the player with accelerating and deccelerating velocity
 	velocity = move_and_slide(velocity,Vector3.UP)
 	$MeshInstance.rotate_z(deg2rad(rotateVelocity.z))
 	$MeshInstance.rotate_x(deg2rad(rotateVelocity.x))
+	#######################################
+	
+	
+	##########Jumping & Gravity Control##########
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMPPOWER
+	velocity.y -= GRAVITY * delta
+	#############################################
