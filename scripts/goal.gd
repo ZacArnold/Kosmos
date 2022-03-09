40ab359e1f4b3c69eb105f35c96252e69b3a24a2
Thy Creeper King <67208693+ThyCreeperKing@users.noreply.github.com>
2022-03-09 11:33:08 -0800

    Mar 9 - level resize and wipe, goal setup for multiple levels

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -1,5 +1,7 @@
 extends Area
 
+export (String, FILE) var nextLevel = "res://levels/level_2.tscn"
+
 func _on_goal_body_entered(body):
 	if body.name == "player":
-		get_tree().change_scene("res://levels/level_2.tscn")
+		get_tree().change_scene(nextLevel)
