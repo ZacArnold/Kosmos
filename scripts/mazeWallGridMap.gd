fbfdb28dd1c61c5995f225a201e313ad64d905d9
Thy Creeper King <67208693+ThyCreeperKing@users.noreply.github.com>
2022-04-05 09:52:18 -0700

    Apr 5 - Level 3 and levelstart fix

--- scripts/mazeWallGridMap.gd
+++ scripts/mazeWallGridMap.gd
@@ -8,4 +8,6 @@ func _on_levelStart_body_entered(body):
 			if get_tree().current_scene.name == "level_1":
 				$animationPlayer.play("wallRise")
 			if get_tree().current_scene.name == "level_2":
-				$animationPlayer.play("wallRise2")
+				$animationPlayer.play("wallRise_2")
+			if get_tree().current_scene.name == "level_3":
+				$animationPlayer.play("wallRise_3")
