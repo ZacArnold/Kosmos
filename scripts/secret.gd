4beb3e101c435af5501c777fdbca4da942be72d4
Thy Creeper King <67208693+ThyCreeperKing@users.noreply.github.com>
2022-04-12 09:46:07 -0700

    Apr 12 - Secret material and hud

--- scripts/secret.gd
+++ scripts/secret.gd
@@ -2,7 +2,14 @@ extends Area
 
 onready var global = get_node("/root/Global")
 
+func _ready():
+	$meshInstance.visible = false
+
 func _on_secret_body_entered(body):
 	if body.name == "player":
 		queue_free()
 		global.secretsFound += 1
+
+func _on_levelStart_body_entered(body):
+	if body.name == "player":
+		$meshInstance.visible = true
