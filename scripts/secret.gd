b16cf82a2361a547e527378f40cda991dea291f3
Thy Creeper King <67208693+ThyCreeperKing@users.noreply.github.com>
2022-04-12 10:36:42 -0700

    Apr 12 - Secret Light

--- scripts/secret.gd
+++ scripts/secret.gd
@@ -2,6 +2,9 @@ extends Area
 
 onready var global = get_node("/root/Global")
 
+func _ready():
+	visible = false
+
 func _on_secret_body_entered(body):
 	if body.name == "player":
 		queue_free()
@@ -14,3 +17,4 @@ func _on_levelStart_body_entered(body):
 func _process(_delta):
 	rotate_x(deg2rad(2))
 	rotate_y(deg2rad(3))
+	rotate_z(deg2rad(1))
