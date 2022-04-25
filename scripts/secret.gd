e65a33fecedd142a066531cb496d9d3c519e85f7
Thy Creeper King <67208693+ThyCreeperKing@users.noreply.github.com>
2022-04-25 07:06:15 -0700

    Apr 25
    Secret collect sfx

--- scripts/secret.gd
+++ scripts/secret.gd
@@ -8,7 +8,9 @@ func _ready():
 
 func _on_secret_body_entered(body):
 	if body.name == "player":
-		queue_free()
+		$pickupConfirm.play()
+		$meshInstance.visible = false
+		$directionalLight.light_energy = 0
 		global.secretFound = true
 
 func _on_levelStart_body_entered(body):
@@ -19,3 +21,6 @@ func _process(_delta):
 	rotate_x(deg2rad(2))
 	rotate_y(deg2rad(3))
 	rotate_z(deg2rad(1))
+
+func _on_pickupConfirm_finished():
+	queue_free()
