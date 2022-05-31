bec05df07dce032d7e112ef53cdde6b8b76711fd
Thy Creeper King <67208693+ThyCreeperKing@users.noreply.github.com>
2022-05-31 09:45:33 -0700

    Delete startButton.gd

--- scripts/startButton.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-#Start screen button
-func _on_Button_pressed():
-	if get_tree().current_scene.name == "startScreen":
-		# warning-ignore:return_value_discarded
-		get_tree().change_scene("res://levels/level_1.tscn")
