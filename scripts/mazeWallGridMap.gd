736c3da37fdebb2fcc8832d65862edf01cf842cd
Thy Creeper King <67208693+ThyCreeperKing@users.noreply.github.com>
2022-06-08 11:29:39 -0700

    Jun 8 - level start sound effect and sfx volume balancing

--- scripts/mazeWallGridMap.gd
+++ scripts/mazeWallGridMap.gd
@@ -5,9 +5,16 @@ func _on_levelStart_body_entered(body):
 		if global.controlAllowed == false:
 			if get_tree().current_scene.name == "level_1":
 				$animationPlayer.play("wallRise")
+				$audioStreamPlayer.play()
 			if get_tree().current_scene.name == "level_2":
 				$animationPlayer.play("wallRise_2")
+				$audioStreamPlayer.play()
 			if get_tree().current_scene.name == "level_3":
 				$animationPlayer.play("wallRise_3")
+				$audioStreamPlayer.play()
 			if get_tree().current_scene.name == "level_4":
 				$animationPlayer.play("wallRise_4")
+				$audioStreamPlayer.play()
+			if get_tree().current_scene.name == "level_5":
+				$animationPlayer.play("wallRise_5")
+				$audioStreamPlayer.play()
